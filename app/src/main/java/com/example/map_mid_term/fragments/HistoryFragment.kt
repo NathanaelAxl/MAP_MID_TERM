b3548ac00b0220d83fde9b7081b552b43be442a0
NimaTamren <ntamren@gmail.com>
2025-10-15 22:54:27 +0700

    Commit Nima, Home

--- app/src/main/java/com/example/map_mid_term/fragments/HistoryFragment.kt
+++ app/src/main/java/com/example/map_mid_term/fragments/HistoryFragment.kt
@@ -5,13 +5,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.map_mid_term.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.map_mid_term.adapters.TransactionAdapter
+import com.example.map_mid_term.databinding.FragmentHistoryBinding
+import com.example.map_mid_term.model.Transaction
 
 class HistoryFragment : Fragment() {
+
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_history, container, false)
+    ): View {
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val transactionList = arrayListOf<Transaction>()
+        transactionList.add(Transaction("Setoran Sukarela", "15 Okt 2025", 50000.0, "credit"))
+        transactionList.add(Transaction("Bayar Angsuran", "10 Okt 2025", 500000.0, "debit"))
+        transactionList.add(Transaction("Penarikan Tunai", "02 Okt 2025", 100000.0, "debit"))
+        transactionList.add(Transaction("Simpanan Wajib", "01 Okt 2025", 150000.0, "credit"))
+        transactionList.add(Transaction("Setoran Sukarela", "28 Sep 2025", 75000.0, "credit"))
+        transactionList.add(Transaction("Tarik Tunai", "20 Sep 2025", 200000.0, "debit"))
+
+        val transactionAdapter = TransactionAdapter(transactionList)
+
+        binding.rvHistory.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = transactionAdapter
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/map_mid_term/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/map_mid_term/fragments/HomeFragment.kt
@@ -5,11 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.map_mid_term.R
-import com.example.map_mid_term.adapters.TransactionAdapter
 import com.example.map_mid_term.databinding.FragmentHomeBinding
-import com.example.map_mid_term.model.Transaction
 
 class HomeFragment : Fragment() {
 
@@ -39,25 +36,7 @@ class HomeFragment : Fragment() {
             }
             isBalanceVisible = !isBalanceVisible
         }
-
-        setupRecyclerView()
     }
-
-    private fun setupRecyclerView() {
-        val transactionList = arrayListOf<Transaction>()
-        transactionList.add(Transaction("Setoran Sukarela", "15 Okt 2025", 50000.0, "credit"))
-        transactionList.add(Transaction("Bayar Angsuran", "10 Okt 2025", 500000.0, "debit"))
-        transactionList.add(Transaction("Penarikan Tunai", "02 Okt 2025", 100000.0, "debit"))
-        transactionList.add(Transaction("Simpanan Wajib", "01 Okt 2025", 150000.0, "credit"))
-
-        val transactionAdapter = TransactionAdapter(transactionList)
-
-        binding.rvTransactions.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = transactionAdapter
-        }
-    }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
