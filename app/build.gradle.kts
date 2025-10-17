eee7dc7ded0407a0e11e6644e607d9703895e0a1
ncholasandre <andrenick1208@gmail.com>
2025-10-17 21:43:29 +0700

    bagusin UI dikit ahhhhhhhhhh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,9 @@ android {
 dependencies {
     val nav_version = "2.8.3"
 
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
